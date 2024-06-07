5926a817d91c068faf70052c0fe416a28d8cfd65
Yul <montazstroy@inbox.ru>
2024-06-07 17:58:11 +0300

    add src1

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,8 +1,6 @@
 public class BmiService {
-    public int calculate(int a, int b) {
-        int result = (b / a^2);
-        int iresult = (int) result;
-        int iresult;
-        return iresult;
+    public int calculate(int weightInKg, double heightInMeters) {
+        double bmi = weightInKg / heightInMeters / heightInMeters;
+        return (int) bmi;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double h = 1.87; //рост
-        int a = (int) h;
-        System.out.println(a);
-        int w = 98; //вес
-        int b = (int) w;
-        System.out.println(b);
-        int iresult = service.calculate(iresult); // должно получиться 28
-        System.out.println(iresult);
+        Double heightInMeters = 1.87;
+        int weightInKg = 98;//вес
+        int bmi = service.calculate(weightInKg, heightInMeters); // должно получиться 28
+        System.out.println(bmi);
     }
 }
